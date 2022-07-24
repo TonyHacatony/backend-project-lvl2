1d7d99d9bbe78fb01f85a3ca17544bb8d3308c9a
EVeremenkov <hands.fins@gmail.com>
2022-07-24 14:39:19 +0300

    added arguments and format option for gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,12 +2,15 @@
 
 import { program } from 'commander';
 
-const command = () => {
-    console.log('Diff');
+const action = (filepath1, filepath2, format) => {
+    console.log(`Diff with format between files: ${filepath1} - ${filepath2}. With format ${format}`);
 };
 
 
 program
   .version('0.0.1')
-  .action(command)
+  .argument('<filepath1>', 'filepath for first file')
+  .argument('<filepath2>', 'filepath for second file')
+  .option('-f, --format', 'output format', 'Default format')
+  .action(action)
   .parse(process.argv);
