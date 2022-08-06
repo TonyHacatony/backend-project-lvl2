a07c7c08d02aca74eae20b1dbdb815f89b324cd5
EVeremenkov <hands.fins@gmail.com>
2022-08-06 18:52:48 +0300

    fixed codestyle

--- __tests__/gendiff.js
+++ __tests__/gendiff.js
@@ -1,8 +1,3 @@
-import path from 'path';
-import { getFilesData, getDiff } from '../index.js';
-
-const filePath = ('/__fixtures__/file1.json');
-
 test('path test', () => {
-  expect("path").toEqual("path");
+  expect('path').toEqual('path');
 });
