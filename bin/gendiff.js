b4b614a1c3325b3564d3453f5d3b14b8251e9e71
EVeremenkov <hands.fins@gmail.com>
2022-08-06 18:16:50 +0300

    refactoring

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,29 +1,17 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import path from 'path';
-import { readFileSync } from 'node:fs';
-import parse from '../src/parser.js';
-import getDiff from '../src/differ.js';
-
-const filepathToObject = (filepath) => {
-  const finalFilePath1 = path.resolve(filepath);
-  const file1 = readFileSync(finalFilePath1, 'utf8');
-  return parse(file1, path.extname(filepath));
-};
-
-const action = (filepath1, filepath2) => {
-  const filePaths = [filepath1, filepath2];
-  const objects = filePaths.map(filepathToObject);
-  const [obj1, obj2] = objects;
-  const diff = getDiff(obj1, obj2);
-  console.log(diff);
-};
+import { getDiff, getFilesData } from '../index.js';
 
 program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .argument('<filepath1>', 'filepath for first file')
   .argument('<filepath2>', 'filepath for second file')
   .option('-f, --format', 'output format', 'Default format')
-  .action(action)
+  .action((filepath1, filepath2) => {
+    const [data1, data2] = getFilesData(filepath1, filepath2);
+    console.log(getDiff(data1, data2));
+  })
   .parse(process.argv);
---
+++ index.js
@@ -0,0 +1,34 @@
+import path from 'path';
+import { readFileSync } from 'node:fs';
+import parse from './src/parser.js';
+
+const makeRow = (key, value, sign = ' ') => `${sign} ${key}: ${value}\n`;
+
+const handleRow = (acc, key, obj1, obj2) => {
+  const updAcc = (text) => `${acc} ${text}`;
+  if (!(Object.hasOwn(obj1, key))) {
+    return updAcc(makeRow(key, obj2[key], '+'));
+  }
+  if (!(Object.hasOwn(obj2, key))) {
+    return updAcc(makeRow(key, obj1[key], '-'));
+  }
+  if (obj1[key] !== obj2[key]) {
+    return updAcc(`${makeRow(key, obj1[key], '-')} ${makeRow(key, obj2[key], '+')}`);
+  }
+  return updAcc(makeRow(key, obj1[key]));
+};
+
+const filepathToObject = (filepath) => {
+  const resolvedPath = path.resolve(filepath);
+  const file = readFileSync(resolvedPath, 'utf8');
+  return parse(file, path.extname(filepath));
+};
+
+const getDiff = (obj1, obj2) => {
+  const keys = Array.from(new Set([...Object.keys(obj1), ...Object.keys(obj2)])).sort();
+  return `${keys.reduce((acc, key) => handleRow(acc, key, obj1, obj2), '{\n')}}`;
+};
+
+const getFilesData = (filepath1, filepath2) => [filepath1, filepath2].map(filepathToObject);
+
+export { getDiff, getFilesData };
